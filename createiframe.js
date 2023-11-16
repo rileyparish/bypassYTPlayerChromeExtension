a53bf2c06d2ed2727981e3cb558632c4dd554452
rileyparish <parish.riley@gmail.com>
2023-11-15 21:03:56 -0700

    Fixed button not showing up sometimes.

--- createiframe.js
+++ createiframe.js
@@ -1,21 +1,34 @@
-// run this code whenever the youtube page changes
+let buttonID = "createiframeButton";
+
+// this event triggers only the first time youtube loads
+window.addEventListener("load", function(event){
+    injectBypassButton();
+});
+
+// this event triggers every subsequent page load
 document.addEventListener("yt-navigate-finish", function (event) {
-    // the videoID will only be valid if we're on a watch page (nothing happens on the home page)
-    let videoID = getVideoId(window.location.href);
-    if (videoID != null) {
-        injectBypassButton();
-    }
+    injectBypassButton();
 });
 
 function injectBypassButton(){
-    let buttonID = "createiframeButton";
-    // create a button on the page (if one doesn't already exist)
+    // the videoID will only be valid if we're on a watch page
+    let videoID = getVideoId(window.location.href);
+
+    // create the bypass button on the page (if one doesn't already exist)
     if(!document.getElementById(buttonID)){
         const openTabButton = document.createElement("button");
         openTabButton.innerHTML = "Open iframe ↗";
         openTabButton.id = buttonID;
         openTabButton.addEventListener("click", openTab);
+        // this places the button in the top icon bar
         document.getElementById("end").prepend(openTabButton);
+    }
+
+    // disable the button if we're not on a watch page
+    if(videoID) {
+        document.getElementById(buttonID).disabled = false;   
+    } else{
+        document.getElementById(buttonID).disabled = true;
     }
 }
 
@@ -23,7 +36,7 @@ function openTab() {
     let videoID = getVideoId(window.location.href);
     // create html for the new web page which will contain the embedded video
     let html = `<iframe width="1271" height="715" src="https://www.youtube.com/embed/${videoID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
-    // send a message to the background script to open a new tab
+    // send a message to the background script to open a new tab containing the embedded video
     chrome.runtime.sendMessage({ action: "openNewTab", content: html });
 }
 
