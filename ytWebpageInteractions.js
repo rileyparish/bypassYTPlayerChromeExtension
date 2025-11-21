e155adab8b855c12030977c45b45faa979528e85
Riley Parish <parish.riley@gmail.com>
2025-11-21 06:40:50 -0700

    Fixed iframe config error by altering request headers through DNR permissions
    Embed requests from extension webpages are considered illegitimate sources. Needed to alter request headers so format the request properly.

--- ytWebpageInteractions.js
+++ ytWebpageInteractions.js
@@ -1,7 +1,7 @@
 let buttonID = "createiframeButton";
 
 // this event triggers only the first time youtube loads
-window.addEventListener("load", function(event){
+window.addEventListener("load", function (event) {
     injectBypassButton();
 });
 
@@ -10,15 +10,15 @@ document.addEventListener("yt-navigate-finish", function (event) {
     injectBypassButton();
 });
 
-function injectBypassButton(){
+function injectBypassButton() {
     // the videoID will only be valid if we're on a watch page
     let videoID = getVideoId(window.location.href);
 
     // remove the button if it exists
-    if(document.getElementById(buttonID)){
+    if (document.getElementById(buttonID)) {
         document.getElementById(buttonID).remove();
     }
-    
+
     // now recreate the button so it always has the right data for the current page
     const openTabButton = document.createElement("button");
     openTabButton.innerHTML = "Open iframe ↗";
@@ -26,22 +26,21 @@ function injectBypassButton(){
     openTabButton.addEventListener("click", openTab);
     // this places the button in the top icon bar
     document.getElementById("end").prepend(openTabButton);
-    
+
 
     // disable the button if we're not on a watch page
-    if(videoID) {
-        document.getElementById(buttonID).disabled = false;   
-    } else{
+    if (videoID) {
+        document.getElementById(buttonID).disabled = false;
+    } else {
         document.getElementById(buttonID).disabled = true;
     }
 }
 
 function openTab() {
     let videoID = getVideoId(window.location.href);
-    // create html for the new web page which will contain the embedded video
-    let html = `<iframe width="1271" height="715" src="https://www.youtube.com/embed/${videoID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
-    // send a message to the background script to open a new tab containing the embedded video
-    chrome.runtime.sendMessage({ action: "openNewTab", content: html });
+    // send a message to the background script to open a new tab which holds the video
+    chrome.runtime.sendMessage({ action: "openNewTab", content: videoID });
+    return;
 }
 
 // parse the url for the videoID (courtesy of the "Return YouTube Dislike" extension)
@@ -51,9 +50,9 @@ function getVideoId(url) {
     if (pathname.startsWith("/clip")) {
         return document.querySelector("meta[itemprop='videoId']").content;
     } else {
-          if (pathname.startsWith("/shorts")) {
+        if (pathname.startsWith("/shorts")) {
             return pathname.slice(8);
-          }
+        }
         return urlObject.searchParams.get("v");
     }
 }
